66b3d802bffe1e1c58148d11ac3fa81cfa40fcb3
klyuevaa <python.klyuev@gmail.com>
2021-11-29 15:58:03 +0300

    ex.2-done

--- 2_files.py
+++ 2_files.py
@@ -16,7 +16,16 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('referat.txt','r', encoding='utf-8') as string:
+        content = string.read()
+    print("длина получившейся строки =", len(content))
+    count = 0
+    for word in content.split(' '):
+        count += 1
+    print("количество слов в тексте =", count)
+    content = content.replace(".","!")
+    with open('referat2.txt', 'w', encoding='utf-8') as string:
+        string.write(content)
 
 if __name__ == "__main__":
     main()
